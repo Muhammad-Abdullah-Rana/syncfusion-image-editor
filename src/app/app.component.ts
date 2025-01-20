e6029343b5fdd3f4c28ffedcdbb2b8225ce6b904
abdullah.arshad <abdullah.arshad@CMDLHRLTX601>
2025-01-20 04:35:47 -0500

    Init Setup Commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,18 @@
 import { Component } from '@angular/core';
+import { ImageEditorModule } from '@syncfusion/ej2-angular-image-editor'
 import { RouterOutlet } from '@angular/router';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, ImageEditorModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 export class AppComponent {
   title = 'syncfusion-image-editor';
+
+  // Registering Syncfusion license key
+  constructor() {
+  }
 }
--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,8 @@
 import { bootstrapApplication } from '@angular/platform-browser';
 import { appConfig } from './app/app.config';
 import { AppComponent } from './app/app.component';
+import { registerLicense } from '@syncfusion/ej2-base';
 
+registerLicense('Enter license here');
 bootstrapApplication(AppComponent, appConfig)
   .catch((err) => console.error(err));
